f6bff411f684ef451a82f29214804b757cde38f3
Winicios Cavalcante da Silva <winicabe@pop-os.localdomain>
2022-01-12 21:19:50 -0300

    Script useState()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
+function Contador(){
+  console.log(useState())
+  const [contador, setContador] = useState(1);
+  
+  function AddContador(){
+    setContador(contador+1)
+  }
+  return(
+    <div>
+      <h1>{contador}</h1>
+      <button onClick={AddContador}>Adicionar</button>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contador />
     </div>
   );
 }
 
-export default App;
+export default App; 
